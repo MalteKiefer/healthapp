f6889235b59a58da2296d9b30d571cb8c5467871
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-08 20:02:16 +0200

    fix(lint): use block comment for eslint-disable in VitalsChart

--- web/src/pages/vitals/VitalsChart.tsx
+++ web/src/pages/vitals/VitalsChart.tsx
@@ -1,4 +1,4 @@
-// eslint-disable react-refresh/only-export-components
+/* eslint-disable react-refresh/only-export-components */
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
